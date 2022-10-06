7be632ef9c6a2395917e0a882731b79c0c91dd0f
grzegorzsowa86 <112910430+grzegorzsowa86@users.noreply.github.com>
2022-10-06 17:19:26 +0100

    Add files via upload

---
+++ Exercise/Exercise/app.js
@@ -0,0 +1,18 @@
+//Exercise 1
+let Age = 36;
+
+console.log(Age);
+
+//Exercise 2
+let numberOfRabbit =2;
+numberOfRabbit = 3 * numberOfRabbit;
+console.log(numberOfRabbit);
+
+//Exercise 3
+let bodmasExample = 3;
+bodmasExample = (bodmasExample + 5) / 6;
+console.log(bodmasExample);
+
+//Exercise 4
+randomNumber = Math.floor(Math.random() * 6);
+console.log(randomNumber);
---
+++ Exercise/string/app.js
@@ -0,0 +1,33 @@
+//Exercise 1
+const myCountry = 'Poland';
+console.log(myCountry);
+
+//Exercise 2
+const countryLength = myCountry.length;
+console.log(countryLength);
+
+//Exercise 3
+const favouriteFood = 'Pasta';
+const foodNameUpperCase = favouriteFood.toUpperCase();
+console.log(foodNameUpperCase);
+
+//Exercise 4
+const foodNameLowerCase = favouriteFood.toLowerCase();
+console.log(foodNameLowerCase);
+
+//Exercise 5
+let myName = 'Grzegorz';
+let myAge = 36;
+const result = 'Hello my name is ' + myName + ' and I am ' + myAge + ' years old.';
+console.log(result);
+//let myName = 'Anna';
+//let myAge = 31;
+const result2 = 'Hello my name is ' + myName + ' and I am ' + myAge + ' years old.';
+console.log(result2);
+
+//Exercise 6
+const firstName = 'Grzegorz';
+const lastName = 'Sowa';
+
+const result3 = `Welcome ${firstName} ${lastName}, How can I help you?`;
+console.log(result3);
